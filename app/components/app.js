0095865838482e74ac7dc8feae454dde1840b868
Meg Stepp <mcstepp@users.noreply.github.com>
2017-11-30 17:23:19 -0500

    separate app into own component

---
+++ app/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../app.css';
+
+// state 
+// lifecycle event 
+// UI => render view
+
+class App extends Component {
+	render() {
+		return(
+			<div>
+				Hello from React!
+			</div>
+		);
+	}
+}
+
+export default App;
--- app/index.js
+++ app/index.js
@@ -1,20 +1,6 @@
-import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './app.css';
-
-// state 
-// lifecycle event 
-// UI => render view
-
-class App extends Component {
-	render() {
-		return(
-			<div>
-				Hello from React!
-			</div>
-		);
-	}
-}
+import App from './components/app.js';
+//import './app.css';
 
 ReactDOM.render(
 	<App />,

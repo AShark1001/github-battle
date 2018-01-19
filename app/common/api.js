227d10ba19486a96302f62981729864ac8a3098d
Meg Stepp <mcstepp@users.noreply.github.com>
2018-01-18 19:14:00 -0500

    axios, promises, battle algorithm

--- app/common/api.js
+++ app/common/api.js
@@ -1,14 +1,70 @@
 import axios from 'axios';
 
-const API = {
-	fetchPopularRepos(language) {
-		let encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+// rate-limiting github	
+// const ID = "your_client_id";
+// const SECRET = "your_secret_id";
+// const PARAMS = `&client_id=${ID}&client_secret=${SECRET}`;
 
-		return axios.get(encodedURI)
-			.then((response) => {
-				return response.data.items;
-			});
-	}
+function getProfile (username) {
+	return axios.get(`https://api.github.com/users/${username}`)
+		.then((user) => user.data);
 }
 
-export default API;
+function getRepos (username) {
+	return axios.get(`https://api.github.com/users/${username}/repos&per_page=100`);
+}
+
+function getStarCount (repos) {
+	return repos.data.reduce((count, repo) => {
+		return count + repo.stargazers_count;
+	}, 0);
+}
+
+function calculateScore (profile, repos) {
+	let followers = profile.followers;
+	let totalStars = getStarCount(repos);
+
+	return (followers * 3) + totalStars;
+}
+
+function handleError (error) {
+	console.warn(error);
+	return null;
+}
+
+function getUserData (player) {
+	return axios.all([
+		getProfile(player),
+		getRepo(player)
+	]).then((data) => {
+		let profile = data[0];
+		let repos = data[1];
+
+		return {
+			profile,
+			score: calculateScore(profile, repos)
+		}
+	});
+}
+
+function sortPlayers (players) {
+	return players.sort((a,b) => {
+		return b.score - a.score;
+	});
+}
+
+export function	battle (players) {
+	return axios.all(players.map(getUserData))
+	 .then(sortPlayers)
+	 .catch(handleError);
+}
+
+export function fetchPopularRepos (language) {
+	let encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+
+	return axios.get(encodedURI)
+		.then((response) => {
+			return response.data.items;
+		});
+	}
+}
